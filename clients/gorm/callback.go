0477aa3253b720287fd8675939b768a5e2f62975
9d77v <9d77v@9d77v.me>
2018-09-01 12:18:10 +0800

    fix: update db statement

--- clients/gorm/callback.go
+++ clients/gorm/callback.go
@@ -1,9 +1,13 @@
 package gorm
 
 import (
+	"database/sql/driver"
 	"fmt"
+	"reflect"
+	"regexp"
 	"strings"
 	"time"
+	"unicode"
 
 	"github.com/jinzhu/gorm"
 	opentracing "github.com/opentracing/opentracing-go"
@@ -55,7 +59,7 @@ func (c *callbacks) after(scope *gorm.Scope, operation string) {
 	if operation == "" {
 		operation = strings.ToUpper(strings.Split(scope.SQL, " ")[0])
 	}
-	tags.DBStatement.Set(span, fmt.Sprint(gorm.LogFormatter(scope.SQL, scope.SQLVars)...))
+	tags.DBStatement.Set(span, formatSQL(scope))
 	span.SetTag("db.table", scope.TableName())
 	span.SetTag("db.method", operation)
 	span.Finish()
@@ -107,4 +111,67 @@ func updateTimeStampForUpdateCallback(scope *gorm.Scope) {
 	if _, ok := scope.Get("gorm:update_column"); !ok {
 		scope.SetColumn("UpdateTime", time.Now())
 	}
+}
+
+func formatSQL(scope *gorm.Scope) string {
+	var (
+		sql             string
+		formattedValues []string
+	)
+	var (
+		sqlRegexp                = regexp.MustCompile(`\?`)
+		numericPlaceHolderRegexp = regexp.MustCompile(`\$\d+`)
+	)
+	for _, value := range scope.SQLVars {
+		indirectValue := reflect.Indirect(reflect.ValueOf(value))
+		if indirectValue.IsValid() {
+			value = indirectValue.Interface()
+			if t, ok := value.(time.Time); ok {
+				formattedValues = append(formattedValues, fmt.Sprintf("'%v'", t.Format("2006-01-02 15:04:05")))
+			} else if b, ok := value.([]byte); ok {
+				if str := string(b); isPrintable(str) {
+					formattedValues = append(formattedValues, fmt.Sprintf("'%v'", str))
+				} else {
+					formattedValues = append(formattedValues, "'<binary>'")
+				}
+			} else if r, ok := value.(driver.Valuer); ok {
+				if value, err := r.Value(); err == nil && value != nil {
+					formattedValues = append(formattedValues, fmt.Sprintf("'%v'", value))
+				} else {
+					formattedValues = append(formattedValues, "NULL")
+				}
+			} else {
+				formattedValues = append(formattedValues, fmt.Sprintf("'%v'", value))
+			}
+		} else {
+			formattedValues = append(formattedValues, "NULL")
+		}
+	}
+
+	// differentiate between $n placeholders or else treat like ?
+	if numericPlaceHolderRegexp.MatchString(scope.SQL) {
+		sql = scope.SQL
+		for index, value := range formattedValues {
+			placeholder := fmt.Sprintf(`\$%d([^\d]|$)`, index+1)
+			sql = regexp.MustCompile(placeholder).ReplaceAllString(sql, value+"$1")
+		}
+	} else {
+		formattedValuesLength := len(formattedValues)
+		for index, value := range sqlRegexp.Split(scope.SQL, -1) {
+			sql += value
+			if index < formattedValuesLength {
+				sql += formattedValues[index]
+			}
+		}
+	}
+	return sql
+}
+
+func isPrintable(s string) bool {
+	for _, r := range s {
+		if !unicode.IsPrint(r) {
+			return false
+		}
+	}
+	return true
 }
